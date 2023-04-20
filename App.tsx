e9cf9be4b49ee056aeee4127d79cc76f7c6cde75
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2023-04-19 21:17:35 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Text,View } from 'react-native'
+
+const App = () => {
+  return (
+    <View style={ {
+      flex:1,
+      backgroundColor:'red',
+      justifyContent:'center'
+    } } >
+      <Text style={ { fontSize:50,textAlign:'center' } } >Hola mundo</Text>
+    </View>
+  )
+}
+
+export default App
